adbbc4ad2567c322ea9c637cafd6b8fed7dfbc3c
George Marcone <gmarcone@gmail.com>
2026-05-14 17:11:17 -0300

    Dockerizando o Backend | Migrations rodando no docker

--- CoreFlow/CoreFlow.API/Program.cs
+++ CoreFlow/CoreFlow.API/Program.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using FluentValidation;
+using CoreFlow.Application.Behaviors;
 using CoreFlow.Application.Commands;
 using CoreFlow.Application.Validators;
 using CoreFlow.Application.Interfaces;
@@ -9,60 +10,32 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
-// register DI
-var conn = builder.Configuration.GetConnectionString("Default");
-if (!string.IsNullOrWhiteSpace(conn))
+var connectionString = builder.Configuration.GetConnectionString("Default");
+if (!string.IsNullOrWhiteSpace(connectionString))
 {
-    // usa EF Core com a connection string definida (SQL Server)
-    builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlServer(conn));
+    builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
     builder.Services.AddScoped<IUserService, EfUserService>();
 }
 else
 {
-    // fallback in-memory
     builder.Services.AddSingleton<IUserService, InMemoryUserService>();
 }
-// mediatR + validators + pipeline
+
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateUserCommand).Assembly));
 builder.Services.AddValidatorsFromAssemblyContaining<CreateUserCommandValidator>();
-builder.Services.AddTransient(typeof(MediatR.IPipelineBehavior<,>), typeof(CoreFlow.Application.Behaviors.ValidationBehavior<,>));
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
